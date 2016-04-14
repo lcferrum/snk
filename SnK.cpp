2d81c46b3a8d94309135565bb88a14500d4fd181
lcferrum <lcferrum@yandex.com>
2016-04-15 00:46:20 +0300

    Missing bracket for OBSOLETE_MAIN

--- SnK.cpp
+++ SnK.cpp
@@ -22,14 +22,15 @@ extern int _CRT_glob;
 extern "C" void __wgetmainargs(int*, wchar_t***, wchar_t***, int, _startupinfo*);
 
 int main()
+{
 	wchar_t **enpv, **argv;
 	int argc;
 	_startupinfo si;
 	__wgetmainargs(&argc, &argv, &enpv, _CRT_glob, &si);
 #else
 extern "C" int wmain(int argc, wchar_t* argv[])
-#endif
 {
+#endif
 #ifdef HIDDEN
 	Extras::MakeInstance(true, L"Search and Kill");
 #else
